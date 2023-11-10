2431827d5a3b50e7296b8bde24b697ddad9b0fd0
Rob0tWizard <ilia.dementev65@gmail.com>
2023-11-10 17:19:25 +0700

    result -> data

--- service/services/views.py
+++ service/services/views.py
@@ -11,10 +11,9 @@ from services.serializers import SubscriptionSerializer
 class SubscriptionView(ReadOnlyModelViewSet):
     queryset = Subscriptions.objects.all().prefetch_related(
         'plan',  # позволяет доставать все планы в одном запросе избегая n+1
-        Prefetch('client',
-                 queryset=Client.objects.all().select_related('user').only('company_name',
-                                                                           'user__email'))
-    )  # .annotate(price=F('service__full_price') - F('service__full_price') * F('plan__discount_percent') / 100.00)
+        Prefetch('client', queryset=Client.objects.all().select_related('user').only('company_name',
+                                                                                     'user__email')))
+    # .annotate(price=F('service__full_price') - F('service__full_price') * F('plan__discount_percent') / 100.00)
     # annotate переносим к таскам для оптимизации работы
 
     # queryset = Subscriptions.objects.all().prefetch_related('client').prefetch_related('client__user')
@@ -31,19 +30,17 @@ class SubscriptionView(ReadOnlyModelViewSet):
         queryset = self.filter_queryset(self.get_queryset())
         response = super().list(request, *args, *kwargs)
 
-# делаем кэш на тотал прайс
+        # делаем кэш на тотал прайс
 
-        price_cache=cache.get(settings.PRICE_CACHE_NAME)
+        price_cache = cache.get(settings.PRICE_CACHE_NAME)
 
         if price_cache:
             total_price = price_cache
         else:
             total_price = queryset.aggregate(total=Sum('price')).get('total')
-            cache.set(settings.PRICE_CACHE_NAME , total_price, 60 * 60) #кэш ОБЯЗАН стухнуть, нельзя делать его вечным
+            cache.set(settings.PRICE_CACHE_NAME, total_price, 60 * 60)  # кэш ОБЯЗАН стухнуть, нельзя делать его вечным
 
-
-
-        response_data = {'result': response.data}
+        response_data = {'data': response.data}
         response_data['total_amount'] = total_price  # показывает сумму всех подписок
         # queryset.aggregate(total=Sum('price')) - выдает словарь в котром подключен тотал c нужными значениями,
         # а с get вернет сумму всех подписок
